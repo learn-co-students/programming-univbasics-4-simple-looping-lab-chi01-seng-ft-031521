7d8c6838a0354ae6500a6ee034c8940d71ac5b2a
Daniel Lee Beasley <dannyleebeasley@gmail.com>
2021-03-04 15:08:17 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,14 @@
-# Write your methods here
+def loop_message_five_times(message = "Hello World")
+  counter = 0
+  while counter <= 5 do
+  puts message
+  counter += 1
+end
+end
+
+def loop_message_n_times(message = "Hello World", n = 5)
+  while n <= 5 do
+  puts message
+  n += 1
+end
+end
