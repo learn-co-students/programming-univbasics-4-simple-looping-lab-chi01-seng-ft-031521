fb1fecb60f26b86a734d5d62f3a4735e53916742
Daniel Lee Beasley <dannyleebeasley@gmail.com>
2021-03-04 15:39:34 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,14 +1,34 @@
 def loop_message_five_times(message = "Hello World")
   counter = 0
-  while counter <= 5 do
+  while counter < 5 do
   puts message
   counter += 1
-end
+ end
 end
 
 def loop_message_n_times(message = "Hello World", n = 5)
-  while n <= 5 do
+  counter = 0
+  while counter <= n do
   puts message
-  n += 1
+  counter += 1
+ end
 end
+
+def output_array(cheer = ["Hoo!", "Hoo!", "Hoo!", "Hoosiers!"])
+  counter = 0 
+  while cheer[counter] do
+    puts cheer[counter]
+    counter += 1 
+  end
 end
+
+def return_string_array(array)
+  count = 0
+  new_array = []
+  while count < array.length do
+    new_array.push(array[count].to_s)
+    count += 1
+  end
+  new_array
+end
+  
